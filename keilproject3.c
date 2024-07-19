cf5e562ba18f6685ee0ecf6c94a754f4c087b435
Sreejith S <sreejithsreeragam03@gmail.com>
2024-07-19 14:23:59 +0530

    Add files via upload

---
+++ keilproject3.c
@@ -0,0 +1,32 @@
+#include<LPC214X.h>
+void delay(unsigned int count);
+int main()
+{
+     int j=0;
+     IODIR0 |=(1<<2);
+	   IODIR0 |=(1<<3);
+	   IODIR0 |=(1<<1);
+	   while(j<5)
+	   {
+	   IOSET0 |=(1<<2);
+		 delay(200000);
+	   IOCLR0 |=(1<<2);
+		  IOSET0 |=(1<<3);
+		 delay(200000);
+	   IOCLR0 |=(1<<3);
+		  IOSET0 |=(1<<1);
+		 delay(200000);
+	   IOCLR0 |=(1<<1);
+			 j++;
+			
+	   }
+		  while(1)
+	   {
+	  
+		 }
+}
+void delay(unsigned int count)
+{
+	unsigned int i;
+	for(i=0;i<count;i++);
+}
